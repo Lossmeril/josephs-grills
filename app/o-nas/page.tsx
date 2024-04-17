f70f9cff1aded55d097cd2de82bcc508d9f29520
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-04-17 10:39:05 +0200

    move: unstaged files for remote editing

--- app/o-nas/page.tsx
+++ app/o-nas/page.tsx
@@ -1,12 +1,44 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import ContactFormSection from "@/components/contactFormSection";
 import Image from "next/image";
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 const AboutPage = () => {
+  const underlineRef = useRef(null);
+
+  useEffect(() => {
+    const underline = underlineRef.current;
+
+    gsap.fromTo(
+      underline,
+      { width: 0 },
+      {
+        ease: "sine.inOut",
+        width: "100%",
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: underline,
+          start: "center center",
+          end: "center center",
+          scrub: true,
+        },
+      }
+    );
+  });
+
   return (
     <>
       <main className="max-w-[1200px] overflow-x-hidden mx-auto">
         <section className="min-h-[800px] bg-white overflow-x-hidden flex flex-col flex-nowrap justify-center items-center text-center pt-20">
-          <h1 className="text-6xl font-bold leading-[1.25em] mb-8 -mt-4 ">
+          <h1 className="text-6xl font-bold leading-[1.25em] mb-8 -mt-4">
             O nás
           </h1>
           <p className="text-3xl font-bold mb-6">
@@ -17,9 +49,16 @@ const AboutPage = () => {
         </section>
         <section className="min-h-[800px] py-5 bg-white overflow-x-hidden flex flex-row flex-nowrap justify-center items-center gap-40 text-center border-t border-[#ededed]">
           <div className="w-full flex flex-col text-mutedtext-dark">
-            <h2 className="text-4xl font-bold leading-[1.25em] mb-8 -mt-4">
-              Náš příběh
-            </h2>
+            <div className="relative">
+              <h2 className="text-4xl font-bold leading-[1.25em] mb-8 -mt-4 z-20 relative">
+                Náš příběh
+              </h2>
+              <div
+                className="bg-orange opacity-25 h-10 absolute top-[10%] z-0"
+                ref={underlineRef}
+              ></div>
+            </div>
+
             <p className="text-md mb-6">
               V srdci každého kusu kovu, který prošel rukama naší rodinné firmy
               je ukryt příběh. Příběh o tradici, řemeslu a rodinné vášni, která
@@ -60,9 +99,15 @@ const AboutPage = () => {
             />
           </div>
           <div className="w-full flex flex-col">
-            <h2 className="text-4xl font-bold leading-[1.25em] mb-8 -mt-4">
-              Náš příběh
-            </h2>
+            <div className="relative">
+              <h2 className="text-4xl font-bold leading-[1.25em] mb-8 -mt-4 z-20 relative">
+                Náš příběh
+              </h2>
+              <div
+                className="bg-orange opacity-25 h-10 absolute top-[10%] z-0"
+                ref={underlineRef}
+              ></div>
+            </div>
             <p className="text-xl mb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
