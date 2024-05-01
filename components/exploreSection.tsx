31b9e61b55439d0475b35f5a8bd92aab382dd31d
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-05-01 12:43:43 +0200

    style: explore section responsive design

--- components/exploreSection.tsx
+++ components/exploreSection.tsx
@@ -37,16 +37,16 @@ const ExploreSection: React.FC = () => {
   return (
     <section
       ref={footnoteRef}
-      className="h-[750px] bg-white overflow-x-hidden flex flex-col flex-nowrap justify-start items-center text-center pt-16 "
+      className="xl:h-[750px] bg-white overflow-x-hidden flex flex-col flex-nowrap justify-start items-center text-center pt-4 xl:pt-16 px-4 lg:px-0"
     >
       <div className="relative w-10 h-10 mx-auto mb-6">
         <Image src="/img/favicon.png" alt="" fill className="object-contain" />
       </div>
-      <p className="text-xl mb-6">Vítejte v rodině Joseph&apos;s!</p>
+      <p className="text-lg xl:text-xl mb-6">Vítejte v rodině Joseph&apos;s!</p>
       <div className="relative">
         <p
           className={
-            "text-6xl font-bold leading-[1.25em] mb-8 -mt-4 z-20 relative " +
+            "text-3xl xl:text-6xl font-bold leading-[1.25em] mb-2 lg:mb-8 -mt-4 z-20 relative " +
             unbounded.className
           }
         >
@@ -57,13 +57,13 @@ const ExploreSection: React.FC = () => {
           className="bg-orange opacity-25 h-10 absolute top-[10%] z-0"
         ></div>
       </div>
-      <div className="flex flex-row flex-nowrap gap-10 w-[650px] mt-10">
-        <div className="w-1/2">
+      <div className="flex flex-col lg:flex-row flex-nowrap gap-5 lg:gap-10 lg:w-[650px] mt-10 mb-24 lg:mb-0 lg:mt-10">
+        <div className="lg:w-1/2">
           <Button link={""} inverse={false}>
             Do e-shopu &raquo;
           </Button>
         </div>
-        <div className="w-1/2">
+        <div className="lg:w-1/2">
           <Button link={"/grily"} inverse={true}>
             Naše grily &raquo;
           </Button>
