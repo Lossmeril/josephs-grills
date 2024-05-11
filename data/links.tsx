e2c812e48addf4d9ebb5d818dd92180085f994e5
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-05-11 20:12:07 +0200

    feat: global links

---
+++ data/links.tsx
@@ -0,0 +1,20 @@
+type Link = {
+  cs: string;
+  en: string;
+};
+
+export const globalLinks: { [id: string]: Link } = {
+  // DOWNLOADABLE LINKS
+  termsAndConditions: {
+    cs: "https://download.josephsgrills.cz/Obchodni-podminky_Josephs-Grills_CZ.pdf",
+    en: "",
+  },
+  GDPR: {
+    cs: "https://download.josephsgrills.cz/GDPR_Josephs-Grills_CZ.pdf",
+    en: "",
+  },
+  certification: {
+    cs: "https://download.josephsgrills.cz/Certifikat-kovu_Josephs-Grills_CZ.pdf",
+    en: "",
+  },
+};
