0b417e3ece9a3bca5d871dac2d6f8573b36d0231
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-05-21 12:53:35 +0200

    fix: merge conflict issues remnants

--- app/grily/joseph/page.tsx
+++ app/grily/joseph/page.tsx
@@ -127,120 +127,128 @@ const JosephProductPage: React.FC = () => {
               </div>
             </div>
             <div className="w-full lg:w-1/2 pt-10 p-6 lg:p-20 lg:pl-10">
-            <p className="text-xl mb-6 text-center lg:text-left">
-              {t("josephTagline")}
-            </p>
-            <h1
-              className={
-                "text-6xl font-bold leading-[1.25em] mb-8 -mt-4 text-center lg:text-left " +
-                unbounded.className
-              }
-            >
-              Joseph
-            </h1>
-            <p className="text-red text-2xl font-bold text-center lg:text-left mb-2">
-              {t("josephPrice")}
-            </p>
-            <p className="text-white text-md text-center lg:text-left mb-2">
-              <span className="text-red font-bold">
-                + {t("josephAccessoryGratePrice")}
-              </span>{" "}
-              {t("josephAccessoryGrate")}
-            </p>
-            <p className="text-white text-md text-center lg:text-left mb-10">
-              <span className="text-red font-bold">
-                + {t("josephAccessoryCoverPrice")}
-              </span>{" "}
-              {t("josephAccessoryCover")}
-            </p>
-            <p className="mb-6 text-center lg:text-left">
-              {t("josephParagraph1")}
-            </p>
-            <p className="mb-6 text-center lg:text-left">
-              {t("josephParagraph2")}
-            </p>
-            <div className="mt-10 lg:mt-0 flex flex-col items-center justify-center lg:justify-start md:flex-row flex-nowrap gap-4 md:gap-10 mb-20 lg:mb-0">
-              <div className="w-40">
-                <Button link={t("linkEshopJoseph")} inverse={false} blank>
-                  {t("buttonEshop")} &raquo;
-                </Button>
-              </div>
-              <div className="w-40">
-                <Button link={"/grily"} inverse={true}>
-                  {t("buttonOurGrills")} &raquo;
-                </Button>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="border-t border-mutedtext-light overflow-x-hidden flex flex-col flex-nowrap p-6 pb-16 lg:p-20 bg-white text-black mb-0 lg:mb-20">
-          {/* --------- UNIQUE SELLING POINTS --------- */}
-          <div className="w-full flex flex-col lg:flex-row flex-nowrap mb-16">
-            {/* --------- USP 1  --------- */}
-            <div className="w-full lg:w-1/3 flex flex-col items-center py-10 lg:p-8">
-              <PropBox name={""}>
-                <AiOutlineSafetyCertificate size={25} />
-              </PropBox>
-              <h2 className="text-center text-xl font-bold mb-8">
-                {t("josephUSP1")}
-              </h2>
-              <p className="text-mutedtext-dark text-center">
-                {t("josephUSP1Text")}
+              <p className="text-xl mb-6 text-center lg:text-left">
+                {t("josephTagline")}
               </p>
+              <h1
+                className={
+                  "text-6xl font-bold leading-[1.25em] mb-8 -mt-4 text-center lg:text-left " +
+                  unbounded.className
+                }
+              >
+                Joseph
+              </h1>
+              <p className="text-red text-2xl font-bold text-center lg:text-left mb-2">
+                {t("josephPrice")}
+              </p>
+              <p className="text-white text-md text-center lg:text-left mb-2">
+                <span className="text-red font-bold">
+                  + {t("josephAccessoryGratePrice")}
+                </span>{" "}
+                {t("josephAccessoryGrate")}
+              </p>
+              <p className="text-white text-md text-center lg:text-left mb-10">
+                <span className="text-red font-bold">
+                  + {t("josephAccessoryCoverPrice")}
+                </span>{" "}
+                {t("josephAccessoryCover")}
+              </p>
+              <p className="mb-6 text-center lg:text-left">
+                {t("josephParagraph1")}
+              </p>
+              <p className="mb-6 text-center lg:text-left">
+                {t("josephParagraph2")}
+              </p>
+              <div className="mt-10 lg:mt-0 flex flex-col items-center justify-center lg:justify-start md:flex-row flex-nowrap gap-4 md:gap-10 mb-20 lg:mb-0">
+                <div className="w-40">
+                  <Button link={t("linkEshopJoseph")} inverse={false} blank>
+                    {t("buttonEshop")} &raquo;
+                  </Button>
+                </div>
+                <div className="w-40">
+                  <Button link={"/grily"} inverse={true}>
+                    {t("buttonOurGrills")} &raquo;
+                  </Button>
+                </div>
+              </div>
             </div>
-            {/* --------- USP 2  --------- */}
-            <div className="w-full lg:w-1/3 flex flex-col items-center border-t lg:border-l lg:border-t-0 border-mutedtext-light py-10 lg:p-8">
-              <PropBox name={""}>
-                <PiCheckSquareOffset size={25} />
-              </PropBox>
-              <h2 className="text-center text-xl font-bold mb-8">
-                {t("josephUSP2")}
-              </h2>
-              <p className="text-mutedtext-dark text-center">
-                {t("josephUSP2Text")}       
+          </section>
+          <section className="border-t border-mutedtext-light overflow-x-hidden flex flex-col flex-nowrap p-6 pb-16 lg:p-20 bg-white text-black mb-0 lg:mb-20">
+            {/* --------- UNIQUE SELLING POINTS --------- */}
+            <div className="w-full flex flex-col lg:flex-row flex-nowrap mb-16">
+              {/* --------- USP 1  --------- */}
+              <div className="w-full lg:w-1/3 flex flex-col items-center py-10 lg:p-8">
+                <PropBox name={""}>
+                  <AiOutlineSafetyCertificate size={25} />
+                </PropBox>
+                <h2 className="text-center text-xl font-bold mb-8">
+                  {t("josephUSP1")}
+                </h2>
+                <p className="text-mutedtext-dark text-center">
+                  {t("josephUSP1Text")}
+                </p>
+              </div>
+              {/* --------- USP 2  --------- */}
+              <div className="w-full lg:w-1/3 flex flex-col items-center border-t lg:border-l lg:border-t-0 border-mutedtext-light py-10 lg:p-8">
+                <PropBox name={""}>
+                  <PiCheckSquareOffset size={25} />
+                </PropBox>
+                <h2 className="text-center text-xl font-bold mb-8">
+                  {t("josephUSP2")}
+                </h2>
+                <p className="text-mutedtext-dark text-center">
+                  {t("josephUSP2Text")}
+                </p>
+              </div>
+              {/* --------- USP 3  --------- */}
+              <div className="w-full lg:w-1/3 flex flex-col items-center border-t lg:border-l lg:border-t-0 border-mutedtext-light py-10 lg:p-8">
+                <PropBox name={""}>
+                  <PiGear size={25} />
+                </PropBox>
+                <h2 className="text-center text-xl font-bold mb-8">
+                  {t("josephUSP3")}
+                </h2>
+                <p className="text-mutedtext-dark text-center">
+                  {t("josephUSP3Text")}
+                </p>
+              </div>
+            </div>
 
-            {/* --------- USP 3  --------- */}
-            <div className="w-full lg:w-1/3 flex flex-col items-center border-t lg:border-l lg:border-t-0 border-mutedtext-light py-10 lg:p-8">
-              <PropBox name={""}>
-                <PiGear size={25} />
-              </PropBox>
-              <h2 className="text-center text-xl font-bold mb-8">
-                {t("josephUSP3")}
-              </h2>
-              <p className="text-mutedtext-dark text-center">
-                {t("josephUSP3Text")}
-              </p>
-            </div>
-          </div>
-          <h2 className="text-4xl font-bold leading-[1.25em] text-center md:text-left mb-8 -mt-4 ">
-            {t("productParametersHeading")}
-          </h2>
-          <ProductParameter name={t("produtParameterHeight")} value="900 mm" />
-          <ProductParameter
-            name={t("produtParameterWidth")}
-            value="740 mm"
-            inverted
-          />
-          <ProductParameter
-            name={t("productParameterSteelPlateThickness")}
-            value="10 mm"
-          />
-          <ProductParameter
-            name={t("productParameterMaterial")}
-            value={t("productParameterStainlessSteel")}
-            inverted
-          />
-          <ProductParameter name={t("productParameterWeight")} value="244 kg" />
-          <ProductParameter
-            name={t("productParameterWeightOfAccessoryGrate")}
-            value="6 kg"
-            inverted
-          />
-          <ProductParameter
-            name={t("productParameterWeightOfAccessoryCover")}
-            value="8 kg"
-          />
-        </section>
+            <h2 className="text-4xl font-bold leading-[1.25em] text-center md:text-left mb-8 -mt-4 ">
+              {t("productParametersHeading")}
+            </h2>
+            <ProductParameter
+              name={t("produtParameterHeight")}
+              value="900 mm"
+            />
+            <ProductParameter
+              name={t("produtParameterWidth")}
+              value="740 mm"
+              inverted
+            />
+            <ProductParameter
+              name={t("productParameterSteelPlateThickness")}
+              value="10 mm"
+            />
+            <ProductParameter
+              name={t("productParameterMaterial")}
+              value={t("productParameterStainlessSteel")}
+              inverted
+            />
+            <ProductParameter
+              name={t("productParameterWeight")}
+              value="244 kg"
+            />
+            <ProductParameter
+              name={t("productParameterWeightOfAccessoryGrate")}
+              value="6 kg"
+              inverted
+            />
+            <ProductParameter
+              name={t("productParameterWeightOfAccessoryCover")}
+              value="8 kg"
+            />
+          </section>
           <Gallery grillName={"Joseph"} portfolioItems={portfolio} />
         </main>
         <ContactFormSection />
