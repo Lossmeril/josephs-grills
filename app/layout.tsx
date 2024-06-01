be623d54403bb75698c2dd9e1fda0555ad412d23
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-06-01 11:28:09 +0200

    fix: minor locales fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,6 @@ import "./globals.scss";
 
 const cabin = Cabin({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Joseph's | Grills. Simple.",
-  description:
-    "Joseph's — grily, které staví na tradici, kvalitě a jednoduchosti. Objevte řadu našich grilů a připojte se k rodině spokojených nadšenců do grilování.",
-  keywords:
-    "gril, grilování, grily, české grily, český gril, grill, český grill, grilly, české grilly, corten, joseph, joseph grill, joseph gril, joseph grilly, joseph grily, josephs, josephs grill, josephs gril, josephs grilly, josephs grily, joseph's, joseph's grill, joseph's gril, joseph's grilly,  joseph's grily, corten, corten grill, corten gril, corten grilly, corten grily",
-  authors: [{ name: "Michal Špitálský", url: "https://www.lossmeril.art" }],
-};
-
 export default function RootLayout({
   children,
 }: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 const IndexDef = () => {
-  const router = useRouter();
-  router.push("/cs");
-
   return <></>;
 };
 
