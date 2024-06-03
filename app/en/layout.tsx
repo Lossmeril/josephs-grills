bd91f99c05471687b9d214a365bb03688a458c1c
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-06-03 09:47:13 +0200

    feat: more keywords

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   description:
     "Joseph's — grily, které staví na tradici, kvalitě a jednoduchosti. Objevte řadu našich grilů a připojte se k rodině spokojených nadšenců do grilování.",
   keywords:
-    "gril, grilování, grily, české grily, český gril, grill, český grill, grilly, české grilly, corten, joseph, joseph grill, joseph gril, joseph grilly, joseph grily, josephs, josephs grill, josephs gril, josephs grilly, josephs grily, joseph's, joseph's grill, joseph's gril, joseph's grilly,  joseph's grily, corten, corten grill, corten gril, corten grilly, corten grily",
+    "gril, grilování, grily, české grily, český gril, grill, český grill, grilly, české grilly, corten, joseph, joseph grill, joseph gril, joseph grilly, joseph grily, josephs, josephs grill, josephs gril, josephs grilly, josephs grily, joseph's, joseph's grill, joseph's gril, joseph's grilly,  joseph's grily, corten, corten grill, corten gril, corten grilly, corten grily, Joseph's Grills, Josephs Grills, Grills Joseph's, Grills Josephs, Joseph'sGrills, JosephsGrills, Joseph Grills, Joseph Grill, JosephGrill, Grill Joseph, GrillJoseph, Grill Joseph's, GrillJoseph's, Grills Joseph, GrillsJoseph, Grills Joseph's, GrillsJoseph's",
   authors: [{ name: "Michal Špitálský", url: "https://www.lossmeril.art" }],
 };
 
