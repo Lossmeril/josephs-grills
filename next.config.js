641237dd4bc242a51120aa63e0e4d6c57f92224e
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-12-15 14:46:36 +0000

    fix: image optimisation turn off

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/cs',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  images: {
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/cs",
+        permanent: true,
+      },
+    ];
+  },
+};
